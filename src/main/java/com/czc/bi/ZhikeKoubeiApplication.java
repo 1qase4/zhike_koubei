75c1f25bf08b900ff127d21110bee55a90fbb352
zchong <zhangchong@shangquanxiu.com>
2017-09-19 16:17:13 +0800

    Initial commit

---
+++ src/main/java/com/czc/bi/ZhikeKoubeiApplication.java
@@ -0,0 +1,13 @@
+package com.czc.bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class ZhikeKoubeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhikeKoubeiApplication.class, args);
+	}
+}
