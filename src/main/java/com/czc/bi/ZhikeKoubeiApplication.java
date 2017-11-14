9c1f29f80e765e1053f6edc958b1f4998bad1920
fw <540771452@qq.com>
2017-11-14 18:04:22 +0800

    更新为https

--- src/main/java/com/czc/bi/ZhikeKoubeiApplication.java
+++ src/main/java/com/czc/bi/ZhikeKoubeiApplication.java
@@ -1,8 +1,15 @@
 package com.czc.bi;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,6 +18,38 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ImportResource(locations = {"classpath:applicationContext.xml"})
 @EnableScheduling
 public class ZhikeKoubeiApplication {
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+
+			@Override
+			protected void postProcessContext(Context context) {
+
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+		return tomcat;
+	}
+
+	private Connector initiateHttpConnector() {
+
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		//Connector监听的http的端口号
+		connector.setPort(9001);
+		connector.setSecure(false);
+		//监听到http的端口号后转向到的https的端口号
+		connector.setRedirectPort(8443);
+		return connector;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZhikeKoubeiApplication.class, args);
