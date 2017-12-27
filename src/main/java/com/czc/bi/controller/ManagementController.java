39063ad901f66de5d76921973d1ba58121ffea2f
fw <540771452@qq.com>
2017-12-27 16:16:32 +0800

    后台管理

--- src/main/java/com/czc/bi/controller/ManagementController.java
+++ src/main/java/com/czc/bi/controller/ManagementController.java
@@ -1,8 +1,11 @@
 package com.czc.bi.controller;
 
+import com.czc.bi.mapper.CfgUserMapper;
 import com.czc.bi.mapper.PotentialCustMapper;
+import com.czc.bi.pojo.CfgUser;
 import com.czc.bi.pojo.PotentialCust;
 import com.czc.bi.pojo.dto.Result;
+import com.czc.bi.pojo.query.CfgUserQuery;
 import com.czc.bi.pojo.query.PotentialCustQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +17,23 @@ import java.util.List;
  * Created by Administrator on 2017/12/27.
  * 口碑后台管理
  */
-@RestController("management")
+@RestController
+@RequestMapping("/management")
 public class ManagementController {
     @Autowired
     private PotentialCustMapper potentialCustMapper;
+
+    @Autowired
+    private CfgUserMapper cfgUserMapper;
+
+    @RequestMapping("cfgLogin")
+    public Result login(String username,String password){
+        List<CfgUser> cfgUsers = cfgUserMapper.selectByQuery(new CfgUserQuery().setUsername(username).setPassword(password));
+        if (cfgUsers == null || cfgUsers.size() == 0){
+            return new Result().setResult(false);
+        }
+        return new Result(cfgUsers.get(0));
+    }
 
     @RequestMapping("show")
     public Result show(){
